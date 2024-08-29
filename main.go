6675f1471ee8130d186cb9cbeb450fcf0286b12d
botka4aet <shram1992@gmail.com>
2024-08-29 12:46:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,17 +5,12 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"sync"
-	"time"
-	"runtime"
 	"math"
+	"runtime"
+	"time"
 )
 
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
-var letternumbersRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
-
-var mu sync.Mutex
-var tries_counter int
+var baseurl = "https://cdn.townofsins.com/media/assets/images/"
 
 type Semaphore struct {
 	C chan struct{}
@@ -26,9 +21,6 @@ func (s *Semaphore) Acquire() {
 }
 func (s *Semaphore) Release() {
 	<-s.C
-	// mu.Lock()
-	// tries_counter++
-	// mu.Unlock()
 }
 
 var sem = Semaphore{
@@ -38,113 +30,82 @@ var sem = Semaphore{
 func main() {
 	fmt.Println(runtime.NumCPU())
 
-	c := make(chan string)
-	go func() {
-		for url, _ := range check {
-			fmt.Println("Bruteforcing ", url)
-			sgnlCh := make(chan struct{})
-			var link [5]int
-			dtime := time.Now()
-			for {
-				sem.Acquire()
-				go scramble_link(url, link, c, sgnlCh)
-				link[0] = 26
-				var loop bool
-				for i, _ := range link {
-					if link[i] >= 26 {
-						if i == len(link)-1 {
-							loop = true
-							break
-						}
-						link[i+1]++
-						if i+1 == 4 && link[4] < 26 {
-							fmt.Printf("%v Now trying ****%v. Speed - %.2f per second\n",time.Now().Format("[15:04:05]"),string(letterRunes[link[4]]),float64(math.Pow(26, 5))/float64(runtime.NumCPU())/time.Since(dtime).Seconds())
-							dtime = time.Now()
-						}
-						for j := 0; j <= i; j++ {
-							link[j] = 0
-						}
+	for url, _ := range check {
+		fmt.Println("Bruteforcing ", url)
+		sgnlCh := make(chan struct{})
+		var link [5]int
+		var sufix string
+		var letterRunes []rune
+		if strings.HasPrefix(url, "comics_adventure/th/") {
+			sufix = ".webp"
+			letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+		} else if strings.HasPrefix(url, "comics_events/th/") {
+			sufix = ".jpg"
+			letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+		} else if strings.HasPrefix(url, "comics/th/") {
+			sufix = "@2x.webp"
+			letterRunes = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
+		}
+		lenr := len(letterRunes)
+
+		dtime := time.Now()
+		for {
+			sem.Acquire()
+			go scramble_link(url, sufix, &letterRunes, lenr, link, sgnlCh)
+			link[0] = lenr
+			var loop bool
+			for i, _ := range link {
+				if link[i] >= lenr {
+					if i == len(link)-1 {
+						loop = true
+						break
+					}
+					link[i+1]++
+					if i+1 == 4 && link[4] < lenr {
+						fmt.Printf("%v Now trying ****%v. Speed - %.2f per second\n", time.Now().Format("[15:04:05]"), string(letterRunes[link[4]]), float64(math.Pow(26, 4))/float64(runtime.NumCPU())/time.Since(dtime).Seconds())
+						dtime = time.Now()
+					}
+					for j := 0; j <= i; j++ {
+						link[j] = 0
 					}
 				}
-				select {
-				case <-sgnlCh:
-					loop = true
-				default:
-				}
-				if loop {
-					break
-				}
+			}
+			select {
+			case <-sgnlCh:
+				loop = true
+			default:
+			}
+			if loop {
+				break
 			}
 		}
-	}()
-	// go func() {
-	// 	for {
-	// 		time.Sleep(time.Minute)
-	// 		c <- ""
-	// 	}
-
-	// }()
-	fi, err := os.OpenFile("txtfiles\\links.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
-	if err != nil {
-		panic(err)
-	}
-	defer fi.Close()
-	for {
-		text := <-c
-		switch text {
-		case "":
-			fmt.Println("Passed 1 min, tries - ", tries_counter*26)
-			mu.Lock()
-			tries_counter = 0
-			mu.Unlock()
-		default:
-			_, err = fi.WriteString(text + "\n")
-			if err != nil {
-				panic(err)
-			}
-		}
-
 	}
 }
 
-func scramble_link(url string, link [5]int, c chan string, sgnlCh chan struct{}) {
+func scramble_link(url string, sufix string, runes *[]rune, lenr int, link [5]int, sgnlCh chan struct{}) {
 	defer sem.Release()
-	for i := 0; i < 26; i++ {
-		select {
-		case <-sgnlCh:
-			return
-		default:
-		}
+	for i := 0; i < lenr; i++ {
 		link[0] = i
-
 		var nlink string
 		for _, t := range link {
-			nlink += string(letterRunes[t])
+			nlink += string((*runes)[t])
 		}
-		check_url(url+"_"+nlink, c, sgnlCh)
+		check_url(url+"_"+nlink+sufix, sgnlCh)
 	}
 }
 
-func check_url(link string, c chan string, sgnlCh chan struct{}) {
-	baseurl := "https://cdn.townofsins.com/media/assets/images/"
-	postfix := ""
-	if strings.HasPrefix(link, "comics_adventure/th/") {
-		postfix = ".webp"
-	} else if strings.HasPrefix(link, "comics_events/th/") {
-		postfix = ".jpg"
-		//	} else if strings.HasPrefix(link, "comics/th/") {
-		//		postfix = "@2x.webp"
-	}
-	if postfix == "" {
-		return
-	}
+func check_url(link string, sgnlCh chan struct{}) {
 	var client = &http.Client{}
 	var res *http.Response
 	for res == nil {
-		res, _ = client.Head(baseurl + link + postfix)
+		res, _ = client.Head(baseurl + link)
 		if res != nil && res.StatusCode == 200 {
 			fmt.Println("Solved: " + link)
-			c <- (link + postfix)
+			fi, err := os.OpenFile("txtfiles\\links.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0777)
+			if err == nil {
+				_, _ = fi.WriteString(link + "\n")
+			}
+			defer fi.Close()
 			close(sgnlCh)
 			return
 		} else if res != nil && res.StatusCode == 500 {
