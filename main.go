0f7ff1145c324adfa12c32dfc4cc277194ec1e00
botka4aet <shram1992@gmail.com>
2024-09-04 11:23:00 +0300

    Ошибка при подключении
    После создания подключения, отправки или получения данных - проверка на наличие ошибки, вызывающей панику

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"crypto/tls"
+	"fmt"
+	"net"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
 	"time"
-	"net"
 )
 
 var mutex = &sync.Mutex{}
@@ -53,54 +53,64 @@ func main() {
 
 			dtime := time.Now()
 			var counter int
-			client, _ := tls.DialWithDialer(&net.Dialer{Timeout: 5*time.Second},"tcp", "cdn.townofsins.com:443", &tls.Config{})
+			conn, err := tls.DialWithDialer(&net.Dialer{Timeout: 5 * time.Second}, "tcp", "cdn.townofsins.com:443", &tls.Config{})
 			ch := make(chan string, 10)
 			go ch_scramble("", &letterRunes, 4, ch)
 
 			var answer string
+			buf := make([]byte, 1000)
 
 			for {
 				result := <-ch
 				for {
-					data := []byte("HEAD /media/assets/images/" + url +"_"+ result + suffix + " HTTP/1.1\r\nHost: cdn.townofsins.com\r\n\r\n")
-					client.Write(data)
-					buf := make([]byte, 250)
-					client.Read(buf)
+					data := []byte("HEAD /media/assets/images/" + url + "_" + result + suffix + " HTTP/1.1\r\nHost: cdn.townofsins.com\r\n\r\n")
+					if strings.HasPrefix(answer, "\x00") || err != nil {
+						conn, err = tls.DialWithDialer(&net.Dialer{Timeout: 5 * time.Second}, "tcp", "cdn.townofsins.com:443", &tls.Config{})
+					}
+					if err != nil {
+						continue
+					}
+					_, err = conn.Write(data)
+					if err != nil {
+						continue
+					}
+					_, err = conn.Read(buf)
+					if err != nil {
+						continue
+					}
 					answer = string(buf[:])
-					if strings.HasPrefix(answer, "\x00") {
-						client, _ = tls.Dial("tcp", "cdn.townofsins.com:443", &tls.Config{})
-					} else if strings.HasPrefix(answer, "HTTP/1.1 500") {
+					if strings.HasPrefix(answer, "HTTP/1.1 500") {
 						break
 					} else if strings.HasPrefix(answer, "HTTP/1.1 200") {
-							close(ch)
-							mutex.Lock()
-							_, _ = fi.WriteString(url +"_"+ result + suffix + "\n")
-							mutex.Unlock()
-							result = "zzzzz"
+						close(ch)
+						mutex.Lock()
+						_, _ = fi.WriteString(url + "_" + result + suffix + "\n")
+						mutex.Unlock()
+						result = "zzzzz"
 						break
 					}
 				}
-				if result == "zzzzz" { 
-					break 
-				} else if counter == 10000 { 
-					fmt.Printf("%v[%v]Speed - %.2f per second\n", time.Now().Format("[15:04:05]"), url, 10000/time.Since(dtime).Seconds())
-					dtime = time.Now()
+				if result == "zzzzz" {
+					break
+				} else if counter == 10000 {
 					counter = 0
+					fmt.Printf("%v[%v]Speed - %.2f per second.\n", time.Now().Format("[15:04:05]"), url, 10000/time.Since(dtime).Seconds())
+					dtime = time.Now()
 				} else {
-					 counter++
+					counter++
 				}
 			}
 		}()
 	}
 }
 
-func ch_scramble(suffix string, runes *[]rune, step int,Ch chan string){
+func ch_scramble(suffix string, runes *[]rune, step int, Ch chan string) {
 	var i int
 	for i < len(*runes) {
 		if step > 0 {
 			ch_scramble(string((*runes)[i])+suffix, runes, step-1, Ch)
 		} else {
-			Ch<-string((*runes)[i])+suffix
+			Ch <- string((*runes)[i]) + suffix
 		}
 		i++
 	}
